3f1598e39dc9aa03c26a59d1375cf39178a060d1
Liam Hackett <liamjhackett@gmail.com>
2015-07-15 15:30:22 +0100

    Change Shader
    Changer shader data to incorporate light

--- app/src/main/java/liamhackett/com/opengles20basiclighting/OpenGLRenderer.java
+++ app/src/main/java/liamhackett/com/opengles20basiclighting/OpenGLRenderer.java
@@ -140,19 +140,38 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer{
         Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
 
         final String vertexShader =
-                "uniform mat4 u_MVPMatrix;      \n"		// A constant representing the combined model/view/projection matrix.
+                "uniform mat4 u_MVPMatrix;      \n"     // A constant representing the combined model/view/projection matrix.
+                        + "uniform mat4 u_MVMatrix;       \n"     // A constant representing the combined model/view matrix.
+                        + "uniform vec3 u_LightPos;       \n"     // The position of the light in eye space.
+                        +"uniform mat4 u_MVPMatrix;      \n"		// A constant representing the combined model/view/projection matrix.
 
                         + "attribute vec4 a_Position;     \n"		// Per-vertex position information we will pass in.
                         + "attribute vec4 a_Color;        \n"		// Per-vertex color information we will pass in.
+                        + "attribute vec3 a_Normal;       \n"
 
                         + "varying vec4 v_Color;          \n"		// This will be passed into the fragment shader.
 
                         + "void main()                    \n"		// The entry point for our vertex shader.
                         + "{                              \n"
-                        + "   v_Color = a_Color;          \n"		// Pass the color through to the fragment shader.
-                        // It will be interpolated across the triangle.
-                        + "   gl_Position = u_MVPMatrix   \n" 	// gl_Position is a special variable used to store the final position.
-                        + "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in
+
+                        // Transform the vertex into eye space.
+                        + "   vec3 modelViewVertex = vec3(u_MVMatrix * a_Position);              \n"
+                        // Transform the normal's orientation into eye space.
+                        + "   vec3 modelViewNormal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));     \n"
+                        // Will be used for attenuation.
+                        + "   float distance = length(u_LightPos - modelViewVertex);             \n"
+                        // Get a lighting direction vector from the light to the vertex.
+                        + "   vec3 lightVector = normalize(u_LightPos - modelViewVertex);        \n"
+                        // Calculate the dot product of the light vector and vertex normal. If the normal and light vector are
+                        // pointing in the same direction then it will get max illumination.
+                        + "   float diffuse = max(dot(modelViewNormal, lightVector), 0.1);       \n"
+                        // Attenuate the light based on distance.
+                        + "   diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));  \n"
+                        // Multiply the color by the illumination level. It will be interpolated across the triangle.
+                        + "   v_Color = a_Color * diffuse;                                       \n"
+                        // gl_Position is a special variable used to store the final position.
+                        // Multiply the vertex by the matrix to get the final point in normalized screen coordinates.
+                        + "   gl_Position = u_MVPMatrix * a_Position;                           \n"
                         + "}                              \n";    // normalized screen coordinates.
 
         final String fragmentShader =
